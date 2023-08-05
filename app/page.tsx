68d201d5a80515955498703d9c7dac89cd05467e
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-08-05 15:42:52 -0300

    add LandingPage first section

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,30 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.scss";
+import { logo, mouse } from "./_assets/";
+import Image from "next/image";
+// import PrimaryButton from "./_components/PrimaryButton";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <section className={styles.landing}>
+        <div className={styles.logo}>
+          <Image src={logo} alt="VeriTrust logo" />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        <div className={styles.intro_texts}>
+          <h1>Licita con confianza, VeriTrust te respalda</h1>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Reinventamos el sistema de licitaciones brindando mayor seguridad,
+            transparencia y confianza en los procesos de contratación.
           </p>
-        </a>
-      </div>
+        </div>
+        <button className={styles.button} type="button">
+          Ingresar a la app
+        </button>
+        <div className={styles.scroll_text}>
+          <p>Haga scroll para ver mas</p>
+          <Image src={mouse} alt="mouse icon" />
+        </div>
+      </section>
     </main>
-  )
+  );
 }
