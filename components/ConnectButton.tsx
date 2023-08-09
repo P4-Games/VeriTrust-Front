54cb083f3ecd78e4492d894c2b8d7edc6d309330
Tomas M. Di Mauro <info@tmdm.com.ar>
2023-08-08 22:54:52 -0300

    Minor code changes

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -1,35 +1,25 @@
-import { IconArrowLeft, IconCurrencyEthereum, IconDownload, IconLink, IconUnlink, IconWallet, IconX } from '@tabler/icons-react';
-import {
-  useAccount,
-  useConnect,
-  useDisconnect,
-  useEnsAvatar,
-  useEnsName,
-} from 'wagmi'
 import styles from "./Button.module.scss";
+
+import { IconCurrencyEthereum, IconUnlink, IconWallet } from '@tabler/icons-react';
+import { useAccount, useDisconnect, useEnsName } from 'wagmi';
 import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Button } from './Button';
+import { ConnectDrawer } from './ConnectDrawer';
 
 
 export function ConnectButton() {
-  const { address, connector, isConnected } = useAccount();
+  const { address, isConnected } = useAccount();
   const { data: ensName } = useEnsName({ address })
-  const { connect, connectors, error, isLoading, pendingConnector } =
-    useConnect()
+  
   const { disconnect } = useDisconnect()
 
   const [showUnlink, setShowUnlink] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
-  const handleDisconnect = async () => {
-    disconnect()
-  }
-
+  const handleDisconnect = async () => disconnect();
   const handleMouseEnter = () => setShowUnlink(true);
   const handleMouseLeave = () => setShowUnlink(false);
-
-  const [openModal, setOpenModal] = useState<boolean>(false);
-  
   const toggleModal = ()=> setOpenModal(!openModal);
 
   if (isConnected) {
@@ -65,66 +55,13 @@ export function ConnectButton() {
   return (
     <div>
       <div onClick={toggleModal}>
-        <Button>
+        <Button
+          type='alt'
+        >
           Connect wallet <IconWallet />
         </Button>
       </div>
-      <AnimatePresence>
-        {
-          !openModal ? (
-            <motion.div
-              initial={{opacity: 0}}
-              animate={{opacity: 1}}
-              exit={{opacity: 0}}
-              className={styles.drawerOverlay}
-            />
-          ): null
-        } 
-      </AnimatePresence>
-      <AnimatePresence>
-        {
-          !openModal ? (
-            <motion.div
-              initial={{width: 0}}
-              animate={{width: "min(500px, 80%)"}}
-              exit={{width: 0}}
-              className={styles.drawer}
-            >
-              <h3 className={styles.drawer_title}>
-                Elige tu billetera
-              </h3>
-              <section className={styles.drawer_button}>
-                {connectors.map((connector) => (
-                  <button
-                    disabled={!connector.ready}
-                    key={connector.id}
-                    onClick={() => connect({ connector })}
-                  >
-                    {connector.name}
-                    {!connector.ready && ' (unsupported)'}
-                    {isLoading &&
-                      connector.id === pendingConnector?.id &&
-                      ' (connecting)'}
-                  </button>
-                ))}
-              </section>
-              <section className={styles.drawer_download}>
-                <a href="https://metamask.io/download.html" target="_blank">
-                  No tienes billetera? Descargue Metamask haciendo click acá
-                  <IconDownload />
-                </a>
-              </section>
-              <button className={styles.drawer_close} onClick={toggleModal}>
-                <IconArrowLeft /> Cerrar
-              </button>
-
-            </motion.div>
-          ): null
-        }
-      </AnimatePresence>
-      
-
-      {error && <div>{error.message}</div>}
+      <ConnectDrawer openModal={openModal} toggleModal={toggleModal} />
     </div>
   )
 }
