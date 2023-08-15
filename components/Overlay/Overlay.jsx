6a8b658360ad09b464064963231fa25e989fd528
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-08-15 01:26:04 -0300

    add Overlay

---
+++ components/Overlay/Overlay.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Overlay.module.scss";
+
+export default function Overlay({ handleClick }) {
+  return <div className={styles.overlay} onClick={() => handleClick()}></div>;
+}
