1e628fac7e9672510b1098cdbc43581bb1fe54d1
Tomas <info@tmdm.com.ar>
2023-09-24 22:23:11 -0300

    unnecessary comments removed

--- utils/tender.ts
+++ utils/tender.ts
@@ -13,49 +13,43 @@ export const getTender = async (id: string): Promise<Tender> => {
         }
     })
 
-    //const transactionDetails = getTXFromLS(id);
+    const web3 = new Web3(ALCHEMY_API_URL);
+    const settings = {
+        apiKey: "ybGQr_yqjMQV4RHXO_Z2BR8K5TuUwbER",
+        network: Network.ETH_GOERLI,
+    };
 
-    //if (!transactionDetails) {
-        const web3 = new Web3(ALCHEMY_API_URL);
-        const settings = {
-            apiKey: "ybGQr_yqjMQV4RHXO_Z2BR8K5TuUwbER",
-            network: Network.ETH_GOERLI,
+    const alchemy = new Alchemy(settings);
+    const transaction = await alchemy.transact.getTransaction(id);
+    const data = transaction?.data;
+    if (data) {
+        const input = web3.utils.hexToAscii(data).replace(/\u0000/g, "").slice(24);
+        const ipfsHash = input.slice(-46);
+        const title = input.slice(0, -47);
+
+        const transactionDetails = {
+            hash: id,
+            input: {
+                title: title,
+                ipfsHash: ipfsHash,
+            },
         };
 
-        const alchemy = new Alchemy(settings);
-        const transaction = await alchemy.transact.getTransaction(id);
-        const data = transaction?.data;
-        if (data) {
-            const input = web3.utils.hexToAscii(data).replace(/\u0000/g, "").slice(24);
-            const ipfsHash = input.slice(-46);
-            const title = input.slice(0, -47);
+        const ipfsData = await fetch("https://gateway.pinata.cloud/ipfs/" + transactionDetails.input.ipfsHash)
+        
+        const ipfsDataJSON = await ipfsData.json();
 
-            const transactionDetails = {
-                hash: id,
-                input: {
-                    title: title,
-                    ipfsHash: ipfsHash,
-                },
-            };
+        console.log(ipfsDataJSON);
 
-            const ipfsData = await fetch("https://gateway.pinata.cloud/ipfs/" + transactionDetails.input.ipfsHash)
-            
-            const ipfsDataJSON = await ipfsData.json();
+        res = ipfsDataJSON;
+        res.txid = id;
 
-            console.log(ipfsDataJSON);
-
-            res = ipfsDataJSON;
-            res.txid = id;
-
-            if(res.currency && res.currency.includes(", ")){
-                res.currency = res.currency.split(", ")[0];
-            }else{
-                res.currency = "ARS";
-            }
-
-            //saveTXLS(transactionDetails);
+        if(res.currency && res.currency.includes(", ")){
+            res.currency = res.currency.split(", ")[0];
+        }else{
+            res.currency = "ARS";
         }
-    //}
+    }
 
     return res;
 }
